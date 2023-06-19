ec7c4beba56b133467566527e3156440c194793c
treytodd <trey.todd@gmail.com>
2023-06-18 22:50:55 -0700

    logging

--- starterApp/src/main/java/com/example/starterApp/HelloController.java
+++ starterApp/src/main/java/com/example/starterApp/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.starterApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        log.info("getting index");
+
+        String message = "Greetings from Spring Boot!";
+
+        log.info("got index; message={}", message);
+
+        return message;
     }
 
 }
